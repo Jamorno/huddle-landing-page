e760fcad4d47d0424b0a65ad7e2573ee07a1277d
Jamorno <100228770+Jamorno@users.noreply.github.com>
2023-04-19 22:23:48 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
-import Head from '@/components/Head'
+import Heading from '@/components/Head'
 import Content from '@/components/Content'
 import Fotter from '@/components/Fotter'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Huggle landing page</title>
+      </Head>
+    
       <main>
         {/* Heading */}
-        <Head />
+        <Heading />
 
         {/* Content */}
         <Content />
