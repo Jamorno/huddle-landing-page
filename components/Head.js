05b1bbd9e1156db482914269c65a0583bd4bbb32
Jamorno <100228770+Jamorno@users.noreply.github.com>
2023-04-19 22:05:52 +0700

    Update Head.js

--- components/Head.js
+++ components/Head.js
@@ -3,8 +3,8 @@ export default function Head() {
     return (
         <div className="pt-10 pl-10 xl:pl-20">
             <div className="">
-                <img className="xl:hidden" src="/logo.svg" width="150" height="30"></img>
-                <img className="hidden xl:inline-block" src="/logo.svg" width="200" height="30"></img>
+                <img className="xl:hidden" src="/logo.svg" width="150" height="30" alt="logo-image-moblie"></img>
+                <img className="hidden xl:inline-block" src="/logo.svg" width="200" height="30" alt="logo-image-desktop"></img>
             </div>
         </div>
 
